bc3c7dcc99a5e3f512381e62ea83fe0bef517b8f
Lasma Poksane <laasmapoksane@gmail.com>
2024-04-29 21:00:00 +0200

    create a function that generates random 6-characters code

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+const generateRandomCode = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
+
+  for (let i = 0; i < 6; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return code;
+};
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
