0d4aae38c53f96ebc81bf01ce3288b4eab60d768
Lasma Poksane <laasmapoksane@gmail.com>
2024-04-29 17:09:16 +0200

    add configurations for bodyParser, cors and EJS

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 const PORT = process.env.PORT;
 
