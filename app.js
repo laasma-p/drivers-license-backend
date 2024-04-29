f33ff3feb21e99f55c71c5a2107a94419b68fd69
Lasma Poksane <laasmapoksane@gmail.com>
2024-04-29 21:03:32 +0200

    save the generated code into the database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const Code = require("./code");
 
 const app = express();
 
@@ -14,6 +15,17 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/generate-code", async (req, res) => {
+  try {
+    const generatedCode = generateRandomCode();
+    const savedCode = await Code.create({ generated_code: generatedCode });
+    res.status(200).json({ code: savedCode.generated_code });
+  } catch (error) {
+    console.error("Error saving the generated code:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 const generateRandomCode = () => {
