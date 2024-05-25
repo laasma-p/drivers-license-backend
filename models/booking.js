951dfcf5381388f9f3cd9631f0931888ab258eb1
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-25 14:48:38 +0200

    create initial Booking model

---
+++ models/booking.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const Booking = sequelize.define(
+  "booking",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    booking_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    booking_language: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    available_spots: {
+      type: DataTypes.INTEGER,
+      defaultValue: 30,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Booking;
