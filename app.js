a8385bef74350243fed8ddd0e1cd9ef0e757c839
Lasma Poksane <laasmapoksane@gmail.com>
2024-11-20 23:35:00 +0100

    render quiz routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth-routes");
 const adminRoutes = require("./routes/admin-routes");
 const bookingRoutes = require("./routes/booking-routes");
+const quizRoutes = require("./routes/quiz-routes");
 
 const app = express();
 
@@ -16,6 +17,7 @@ const PORT = process.env.PORT;
 app.use("/", authRoutes);
 app.use("/admin", adminRoutes);
 app.use("/booking", bookingRoutes);
+app.use("/quiz", quizRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
