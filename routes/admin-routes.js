af95e9a4e6fd5deec1229b47cda7e90b48f26806
Lasma Poksane <laasmapoksane@gmail.com>
2024-11-20 23:59:55 +0100

    extract admin-related code from app.js to a separate file

---
+++ routes/admin-routes.js
@@ -0,0 +1,70 @@
+const express = require("express");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const Code = require("../models/code");
+const TestTaker = require("../models/test-taker");
+const router = express.Router();
+
+router.post("/generate-code", async (req, res) => {
+  try {
+    const generatedCode = generateRandomCode();
+    const savedCode = await Code.create({ generated_code: generatedCode });
+    res.status(200).json({ code: savedCode.generated_code });
+  } catch (error) {
+    console.error("Error saving the generated code:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const generateRandomCode = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
+
+  for (let i = 0; i < 6; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return code;
+};
+
+router.post("/verify-code", async (req, res) => {
+  const enteredCode = req.body.code;
+
+  try {
+    const codeExists = await Code.findOne({
+      where: { generated_code: enteredCode },
+    });
+
+    if (!codeExists) {
+      return res.status(404).json({ error: "Invalid code" });
+    }
+
+    const testTaker = await TestTaker.findOne({
+      where: { auth_code: null },
+    });
+
+    if (!testTaker) {
+      return res
+        .status(400)
+        .json({ error: "All codes have been used - generate new ones" });
+    }
+
+    await testTaker.update({ auth_code: enteredCode });
+
+    const token = jwt.sign({ userId: testTaker.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      message: "Code verified and assigned to the test taker successfully",
+      token: token,
+      userId: testTaker.id,
+    });
+  } catch (error) {
+    console.error("Error verifying code:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
