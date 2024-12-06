151e9fde23fb6dd1b4bc99cd3dbcb73d8358ad00
Lasma Poksane <laasmapoksane@gmail.com>
2024-12-06 22:56:52 +0100

    update Code model

--- models/code.js
+++ models/code.js
@@ -1,5 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../sequelize");
+const TestTaker = require("./test-taker");
+const Booking = require("./booking");
 
 const Code = sequelize.define(
   "code",
@@ -14,10 +16,27 @@ const Code = sequelize.define(
       allowNull: false,
       unique: true,
     },
+    test_taker_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: TestTaker,
+        key: "id",
+      },
+    },
+    booking_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Booking,
+        key: "id",
+      },
+    },
   },
   {
     timestamps: false,
   }
 );
+
+Code.belongsTo(TestTaker, { foreignKey: "test_taker_id" });
+Code.belongsTo(Booking, { foreignKey: "booking_id" });
 
 module.exports = Code;
